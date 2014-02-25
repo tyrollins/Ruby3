27e897f62cd0af3734bcd02f718615efa0518130
Tyler Rollins <tylerrollins@outlook.com>
2014-02-25 14:03:05 +0000

    refactored and waved loan amount if 75% down

--- loan.rb
+++ loan.rb
@@ -1,8 +1,9 @@
 def provide_mortgage?(salary, deposit, property_value)
   loan_amount = property_value - deposit
-  property_value < 650000 ? min_deposit = 0.05 : min_deposit = 0.2
-  max_multiplier = 5 # how many annual income can be borrowed
-  deposit >= property_value.to_i * min_deposit && 
-      salary * max_multiplier >= loan_amount
+  min_deposit = property_value < 650000 ? 0.05 : 0.2
+  max_multiplier = 5 
+  deposit >= property_value * min_deposit && 
+      (salary * max_multiplier >= loan_amount || 
+      	deposit >= property_value * 0.75)
 end
-puts provide_mortgage?(30_000, 50_000, 200_000)
+puts provide_mortgage?(salary, deposit, property_value)
