7ec19c085cbfc2b9ae1788aa5c37b9e508aa4cec
Tyler Rollins <tylerrollins@outlook.com>
2014-02-25 12:12:31 +0000

    refactored a bit

--- loan.rb
+++ loan.rb
@@ -1,8 +1,8 @@
 def provide_mortgage?(salary, deposit, property_value)
   loan_amount = property_value - deposit
-  min_deposit = 0.2 # 20%
-  max_multiplier = 5 # how many annual incomes can be borrowed
-  deposit >= property_value * min_deposit && 
+  property_value < 650000 ? min_deposit = 0.05 : min_deposit = 0.2
+  max_multiplier = 5 # how many annual income can be borrowed
+  deposit >= property_value.to_i * min_deposit && 
       salary * max_multiplier >= loan_amount
 end
-puts provide_mortgage?(30000, 50000, 200000)
+puts provide_mortgage?(30_000, 50_000, 200_000)
